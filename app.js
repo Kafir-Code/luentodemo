0679a7f40697a90618d6e5a537bab6eed42aae3d
Kafir-Code <sufc@elisanet.fi>
2021-12-17 13:43:54 +0200

    monitorointi 3

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { Application } from "https://deno.land/x/oak@v9.0.1/mod.ts";
 
 const app = new Application();
-app.use((context, next) => {
+app.use(({ request }, next) => {
     const data = { 
         site: "( ͡° ͜ʖ ͡°) Perjantai...", 
         data: { 
