dba2d54f54c77967e5feb5543997b290284bd3e3
Kafir-Code <sufc@elisanet.fi>
2021-12-17 11:32:34 +0200

    app created 4

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import { Application } from "https://deno.land/x/oak@v9.0.1/mod.ts";
 
 const app = new Application();
 app.use((context, next) => {
-    context.response.body = "Hello World";
+    context.response.body = "Arto, täähän toimii! :o;
 });
 
 export { app };
