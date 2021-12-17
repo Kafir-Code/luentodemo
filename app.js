0f0b58c836a1750b06fecc89e2fa3f361642edce
Kafir-Code <sufc@elisanet.fi>
2021-12-17 13:24:37 +0200

    monitorointi 1

--- app.js
+++ app.js
@@ -2,6 +2,24 @@ import { Application } from "https://deno.land/x/oak@v9.0.1/mod.ts";
 
 const app = new Application();
 app.use((context, next) => {
+    const data = { 
+        site: "pyttipannu", 
+        data: { method: context.request.method, path: context.request.path 
+        },
+    };
+    
+    fetch("https://pyttipannu.deno.dev/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    body: JSON.stringify(data),
+    });
+
+    next();
+});
+
+app.use((context, next) => {
     context.response.body = "Arto, täähän toimii! :o - Koska syödään??";
 });
 
