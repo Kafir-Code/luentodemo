39fc9969a809ecc026eb087c76225f571b55e6cd
Kafir-Code <sufc@elisanet.fi>
2021-12-17 15:18:54 +0200

    Running in docker

--- app/app.js
+++ app/app.js
@@ -6,30 +6,5 @@ const app = new Application();
 app.use(middlewares.monitoringMiddleware);
 app.use(middlewares.renderMiddleware);
 app.use(router.routes());
-/*
-const app = new Application();
-app.use(({ request }, next) => {
-    const data = { 
-        site: "( ͡° ͜ʖ ͡°) '..Toss a coin to your witcher..'", 
-        data: { 
-            //method: request.method, 
-            path: request.url.pathname,
-        },
-    };
-    
-    fetch("https://deno-simple-monitoring.herokuapp.com/api/entries", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    body: JSON.stringify(data),
-    });
 
-    next();
-});
-
-app.use((context, next) => {
-    context.response.body = "Arto, täähän toimii! :o - Koska syödään??";
-});
-*/
 export { app };
